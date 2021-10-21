57b6bc1d0d4022609084e1180531699ee699ec4a
Mauro Rojas <mauro.rojas@rosariosoftware.com>
2021-10-21 02:14:44 -0300

    Added the app's core

---
+++ src/App.jsx
@@ -0,0 +1,171 @@
+import React, {useState, useEffect} from 'react';
+
+import Header from './components/HeaderSection/Header';
+import ItemRedeem from './components/HeaderSection/Routes/Redeem/ItemRedeem';
+import Filter from './components/Filter/Filter';
+import SubHeader from './components/SubHeader/SubHeader';
+import MainContent from './components/MainContent/MainContent';
+
+import classes from './components/HeaderSection/Header.module.css'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+
+import ItemShopList from './components/HeaderSection/Routes/ShopList/ItemShopList';
+
+function App(){
+
+  /* API */
+  const productsUrl = process.env.REACT_APP_API_URL
+  const userUrl = process.env.REACT_APP_API_URL_USER
+  const pointUrl = process.env.REACT_APP_API_URL_POINT
+  const redeemUrl = process.env.REACT_APP_API_URL_REDEEM
+  const historyUrl = process.env.REACT_APP_API_URL_HISTORY
+  const baseToken = process.env.REACT_APP_API_TOKEN
+
+  /* products state */
+  const [products, setProducts] = useState([])
+  /* user state*/
+  const [user, setUser] = useState([])
+  
+  /* points state */
+  const [currentPoints, setCurrentPoints] = useState(null)
+
+  /* Values of Pagination */
+  const [paginatorLeft, setPaginatorLeft] = useState(0)
+  const [paginatorRight, setPaginatorRight] = useState(15)
+
+  /* Values of Filter */
+  const [nameCategoryFilter, setNameCategoryFilter] = useState('')
+
+  /*Values to screen disposition */
+  const [disposition, setDisposition] = useState('grid')
+
+  /* Shop list */
+  const [shopList, setShopList] = useState([])
+
+  useEffect( () => {
+    getProducts()
+    getUser()
+  }, [])
+
+  const getProducts = async() => {
+    try {
+      const response = await fetch(productsUrl, {
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': `Bearer ${baseToken}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+      })
+      const dataProducts = await response.json()
+      setProducts(dataProducts)
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  const amountProducts = products.length
+  function orderProducts(){
+    switch (nameCategoryFilter) {
+      case 'Highest price':
+        return products.sort(((a,b) => b.cost - a.cost))
+      case 'Lowest price':
+        return products.sort(((a,b) => a.cost - b.cost))
+      default:
+        return products
+    }
+  }
+  const dataProducts = orderProducts()
+
+  /*get User */
+  const getUser = async() => {
+    try {
+      const response = await fetch(userUrl, {
+        method: 'GET',
+        headers: new Headers({
+            'Authorization': `Bearer ${baseToken}`, 
+            'Content-Type': 'application/x-www-form-urlencoded'
+        })
+      })
+      const dataUser = await response.json()
+      setUser(dataUser)
+      setCurrentPoints(dataUser.points)
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  return (
+    <Router>
+      <Header
+        user={user}
+        currentPoints={currentPoints}
+        setCurrentPoints={setCurrentPoints}
+        pointUrl={pointUrl}
+        baseToken={baseToken}
+      />
+      <div 
+        className={classes.container}
+        style={{
+          display: 'flex',
+          flexDirection:'column',
+          justifyContent:'center',
+          alignItems:'center',
+          width:'100%',
+          marginRight:0, 
+        }}
+      >
+        <Switch>
+          <Route path="/" exact>
+            <SubHeader/>
+            <Filter
+              paginatorLeft={paginatorLeft}
+              setPaginatorLeft={setPaginatorLeft}
+              paginatorRight={paginatorRight}
+              setPaginatorRight={setPaginatorRight}
+              amountProducts={amountProducts}
+              nameCategoryFilter={nameCategoryFilter}
+              setNameCategoryFilter={setNameCategoryFilter}
+              disposition={disposition}
+              setDisposition={setDisposition}
+            />
+            <MainContent
+              data={dataProducts}
+              paginatorLeft={paginatorLeft}
+              paginatorRight={paginatorRight}
+              currentPoints={currentPoints}
+              setCurrentPoints={setCurrentPoints}
+              disposition={disposition}
+              user={user}
+              shopList={shopList}
+              setShopList={setShopList}
+              redeemUrl={redeemUrl}
+              baseToken={baseToken}
+            />
+          </Route>
+          <Route path="/shops">
+              <ItemShopList
+                shopList={shopList}
+                currentPoints={currentPoints}
+                setCurrentPoints={setCurrentPoints}
+                redeemUrl={redeemUrl}
+                baseToken={baseToken}
+              />
+          </Route>
+          <Route path="/redeem">
+            <ItemRedeem
+              historyUrl={historyUrl}
+              baseToken={baseToken}
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
