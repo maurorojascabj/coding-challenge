868c9fa625a3969f8bb59f73d0d6c01a09692913
Mauro Rojas <mauro.rojas@rosariosoftware.com>
2021-10-21 01:12:55 -0300

    Added the pagination item to the filter section

---
+++ src/components/Filter/ItemFilter/Pagination/Pagination.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import classes from './Pagination.module.css'
+import iconLeft from '../../../../images/icons/arrow-left.svg'
+import iconRight from '../../../../images/icons/arrow-right.svg'
+
+
+const Pagination = ({paginatorLeft, setPaginatorLeft, paginatorRight, setPaginatorRight, amountProducts}) => {
+
+    const handlerPaginatorLeft = (left, right) => {
+        if(left !== 0){
+            let range = right - left
+            left = left - range
+            setPaginatorLeft(left)
+            right = right - range
+            setPaginatorRight(right)
+        }
+        return
+    }
+    const handlerPaginatorRight = (left, right) => {
+        console.log('Right: ', right)
+        if(right <= amountProducts){
+            let range = right - left
+            left = left + range
+            setPaginatorLeft(left)
+            right = right + range
+            setPaginatorRight(right)
+        }
+        return
+    }
+
+    return (
+        <div className={classes.containerPagination}>
+                <img
+                    src={iconLeft}
+                    alt=""
+                    onClick={()=>handlerPaginatorLeft(paginatorLeft, paginatorRight, setPaginatorLeft, setPaginatorRight)} />
+                <img
+                    src={iconRight}
+                    alt=""
+                    onClick={()=>handlerPaginatorRight(paginatorLeft, paginatorRight, setPaginatorLeft, setPaginatorRight)}
+                />
+        </div>
+    )
+}
+
+export default Pagination
